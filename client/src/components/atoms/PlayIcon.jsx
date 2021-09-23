411fe7ce7f910eb25b72f5f8ee0091c41f3b752f
jaehyeon48 <imjaehyeon48@gmail.com>
2021-09-23 21:21:48 +0900

    fix: play 버튼 아이콘 수정

--- client/src/components/atoms/PlayIcon.jsx
+++ client/src/components/atoms/PlayIcon.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 const PlayIcon = () => {
     return (
-        <svg width="52" height="50" viewBox="0 0 52 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0)">
-                <path d="M49.2607 20.9668L8.40357 0.644575C5.08393 -1.00582 0 0.595747 0 4.67778V45.3125C0 48.9747 4.72411 51.1817 8.40357 49.3458L49.2607 29.0333C52.9054 27.2266 52.917 22.7735 49.2607 20.9668Z" fill="currentColor" />
-            </g>
-            <defs>
-                <clipPath id="clip0">
-                    <rect width="52" height="50" fill="white" />
-                </clipPath>
-            </defs>
+        <svg width="55" height="55" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z">
+            </path>
         </svg>
     );
 }
