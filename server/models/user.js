9a2cb51a95222bc46f411b3e7b28e71734e5dcf9
lee-3-8 <leesinji8@gmail.com>
2021-09-23 21:34:46 +0900

    feat: user schema
    유저 스키마 추가

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true},
+  pwd: {type: String, required: true},
+  sessionid: {type:Number, required:true},
+  total:{type:Number,default:0},
+  today:{type:Number,default:0},
+  timelog:{type:Map}
+});
+
+module.exports = mongoose.model('user', userSchema)
