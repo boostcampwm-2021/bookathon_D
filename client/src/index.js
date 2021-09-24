150b5f391daa076f87c8f94c98224125d46b2126
jaehyeon48 <imjaehyeon48@gmail.com>
2021-09-24 10:01:16 +0900

    feat: beforeunload 이벤트 추가

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,11 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault();
+  e.returnValue = '';
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
