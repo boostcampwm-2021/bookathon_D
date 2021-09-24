d15fb0d703d24bb1da263aa68268f01aa09481d1
lee-3-8 <leesinji8@gmail.com>
2021-09-24 13:39:53 +0900

    feat: 상위 n명 반환 api 추가

--- server/components/timers/index.js
+++ server/components/timers/index.js
@@ -4,5 +4,6 @@ const ctrl = require('./timer.controller');
 router.post('',ctrl.insertTimerCtrl);
 router.get('',ctrl.AllTimerCtrl);
 router.get('/daily',ctrl.dailyTimerCtrl);
+router.get('/top',ctrl.topCtrl);
 
 module.exports = router;
